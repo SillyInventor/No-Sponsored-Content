c15ea53f987e5a98ac36380ac83a8798f8dad6c2
Silly Inventor <jamesdavis3004@gmail.com>
2016-11-10 22:40:10 -0500

    First Creation

---
+++ remover.js
@@ -0,0 +1,40 @@
+function deleteSuggestedContent() {
+  var spans = document.querySelectorAll("span");
+  var sponsName = "Suggested Post";
+  for (var i  = 0; i < spans.length; ++i) {
+    var text = spans[i].textContent;
+    if (text == sponsName)
+    {
+        getTopParent(spans[i]).remove();
+    }
+  }
+};
+
+function deleteSponsoredContent() {
+  var links = document.querySelectorAll("a");
+  var sponsClass = "uiStreamSponsoredLink";
+  for (var i  = 0; i < links.length; ++i) {
+    if (links[i].getAttribute("class") == sponsClass)
+    {
+        getTopParent(spans[i]).remove();
+    }
+  }
+};
+
+function getTopParent(content) {
+    var facebookClass = "userContentWrapper"; 
+    while(!content.hasAttribute("class") || content.getAttribute("class").indexOf(facebookClass)==-1)
+    {
+        content = content.parentElement;
+    }
+    return content.parentElement;
+};
+
+$(window).bind("DOMSubtreeModified", function() {
+    deleteSuggestedContent();
+    deleteSponsoredContent();
+});
+ debugger;
+
+
+//# sourceURL=code.js
